2e453e6bc7d6f73c98af5d94c9c181a38f882e31
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-06-09 19:53:30 +0200

    fix

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,30 +1,25 @@
 import React from "react";
 import ScrollAnimation from "react-animate-on-scroll";
+import Image from "../Images/image.png";
 
 const About = ({ name, p1, p2 }) => {
   return (
-    <div id='about'>
+    <div id="about">
       <div className="cf dt-1 w-100 bt b--black-10 pv4">
         <div className="cf ph3 ph5-ns pv5">
           <header className="fn fl-ns w-50-ns pr4-ns">
             <h1 className="mb3 mt0 lh-title f2 light-gray">About Me</h1>
             <div className="f5 ttu tracked white">{name}</div>
             <div>
-              <ScrollAnimation animateIn='fadeIn'>
-                <img
-                  alt="myPic"
-                  className=""
-                  src={`./Images/images/image.png`}
-                />
+              <ScrollAnimation animateIn="fadeIn">
+                <img alt="myPic" className="" src={Image} />
               </ScrollAnimation>
             </div>
           </header>
           <div className="fn fl-ns w-50-ns mt7">
-            <ScrollAnimation animateIn='fadeInRight'>
-            <h2 className="measure pv4-l mt0-ns light-gray 3h-copy">{p1}</h2>
+            <ScrollAnimation animateIn="fadeInRight">
             </ScrollAnimation>
-            <ScrollAnimation animateIn='fadeInRight'>
-            <h2 className="measure pv4-l mt0-ns light-gray 3h-copy">{p2}</h2>
+            <ScrollAnimation animateIn="fadeInRight">
             </ScrollAnimation>
           </div>
         </div>
--- src/Components/Feature/Feature.js
+++ src/Components/Feature/Feature.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Feature.css";
-import { Scrollchor } from '@seinopsys-forks/react-scrollchor';
+import { Scrollchor } from "@seinopsys-forks/react-scrollchor";
+import logo192 from "../Images/logo192.png";
 
 const Feature = (props) => {
   return (
@@ -16,33 +17,58 @@ const Feature = (props) => {
           <div className="bg-black-80 pv5 pb6-m pb7-l">
             <nav className="dt w-100 mw8 center">
               <div className="dtc w2 v-mid pa3">
-                
                 <Scrollchor
-                  to=''
+                  to=""
                   href="/"
                   className="dib w3 h3 pa1 ba b--white-90 grow-large border-box"
                 >
-                  <img
-                    src={`./Images/images/logo192.png`}
-                    className="pointer"
-                    alt="Site Name"
-                  ></img>
+                  <img src={logo192} className="pointer" alt="Site Name"></img>
                 </Scrollchor>
               </div>
-              <div className='dtc v-mid tr pa3'>
-                <Scrollchor to='#about' className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3" >About</Scrollchor> 
-                <Scrollchor to='#projects' className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3">Projects</Scrollchor>
-                <Scrollchor to='#myLinks' className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3">My Links</Scrollchor>  
+              <div className="dtc v-mid tr pa3">
+                <Scrollchor
+                  to="#about"
+                  className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3"
+                >
+                  About
+                </Scrollchor>
+                <Scrollchor
+                  to="#projects"
+                  className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3"
+                >
+                  Projects
+                </Scrollchor>
+                <Scrollchor
+                  to="#myLinks"
+                  className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3"
+                >
+                  My Links
+                </Scrollchor>
               </div>
             </nav>
             <div className="tc-l mt4 mt5-m mt6-l ph3">
-              <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Luigi Campagnola</h1>
-              <h2 className="fw1 f3 white-80 mt3 mb4">Front-End Web Developer</h2>
-              <a className="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" 
-              href="https://www.linkedin.com/in/luigi-campagnola-0482871a0/"
-              target="_blank" rel="noreferrer">Call to Action</a>
+              <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">
+                Luigi Campagnola
+              </h1>
+              <h2 className="fw1 f3 white-80 mt3 mb4">
+                Front-End Web Developer
+              </h2>
+              <a
+                className="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3"
+                href="https://www.linkedin.com/in/luigi-campagnola-0482871a0/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Call to Action
+              </a>
               <span className="dib v-mid ph3 white-70 mb3">or</span>
-              <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="LuigiCampagnolaCV.pdf" download>View Resume</a>
+              <a
+                className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3"
+                href="LuigiCampagnolaCV.pdf"
+                download
+              >
+                View Resume
+              </a>
             </div>
           </div>
         </div>
